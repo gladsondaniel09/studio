62018c9ffbdd6f2abdfd0579a96024b05fbc4596
Gladson Daniel <gladsondaniel09@gmail.com>
2026-02-10 06:10:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "audit-vision-qxwkt",
+  "appId": "1:216545904065:web:017396cce5560609539b40",
+  "apiKey": "AIzaSyCmI5uDb2jShAwZD4enJRXw-nQ65pUJVDg",
+  "authDomain": "audit-vision-qxwkt.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "216545904065"
+};
